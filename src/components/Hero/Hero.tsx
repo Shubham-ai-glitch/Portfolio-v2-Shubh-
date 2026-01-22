2e1f330c979b74d02677d55bc46b0aab7670849f
Shubham-ai-glitch <shubhamsingh7236002388@gmail.com>
2026-01-22 13:57:38 +0530

    Hero.tsx

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -3,11 +3,12 @@ import { Container } from "./styles"
 import ScrollAnimation from "react-animate-on-scroll"
 import Illustration from "../../assets/illustration.svg"
 import { NavHashLink } from "react-router-hash-link"
-import linkedin from '../../assets/linkedin.svg'
 import githubIcon from '../../assets/github.svg'
 import whatsapp from '../../assets/whatsapp.svg'
 import Hello from '../../assets/Hello.gif'
 import telegram from '../../assets/telegram.svg'
+import instagramIcon from '../../assets/instagram.svg'
+
 export function Hero() {
   return (
     <Container id="home">
@@ -16,13 +17,13 @@ export function Hero() {
           <p>Hello <img src={Hello} alt="Hello" width="20px"/>, I'm</p>
         </ScrollAnimation>
         <ScrollAnimation animateIn="fadeInUp" delay={0.2 * 1000}>
-          <h1>Vinayak Singh</h1>
+          <h1>Shubham Singh</h1>
         </ScrollAnimation>
         <ScrollAnimation animateIn="fadeInUp" delay={0.4 * 1000}>
-          <h3>Software Engineer</h3>
+          <h3>Developer & Hacking Enthusiast</h3>
         </ScrollAnimation>
         <ScrollAnimation animateIn="fadeInUp" delay={0.6 * 1000}>
-          <p className="small-resume">1 Year Experience</p>
+          <p className="small-resume">11th Grade Student | Kanpur</p>
         </ScrollAnimation>
         <ScrollAnimation animateIn="fadeInUp" delay={0.8 * 1000}>
           <BrowserRouter>
@@ -30,39 +31,25 @@ export function Hero() {
           </BrowserRouter>
         </ScrollAnimation>
         <ScrollAnimation animateIn="fadeInUp" delay={1 * 1000}>
-      <div className="social-media"><a
-        href="https://www.linkedin.com/in/codevinayak"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <img src={linkedin} alt="Linkedin" />
-      </a>
-        <a
-          href="https://github.com/CodeVinayak/"
-          target="_blank"
-          rel="noreferrer"
-        >
+      <div className="social-media">
+        <a href="https://github.com/Shubhams872-spec" target="_blank" rel="noreferrer">
           <img src={githubIcon} alt="GitHub" />
         </a>
-        <a
-          href="https://api.whatsapp.com/send/?phone=%2B919630576848&text=Hello+Vinayak+I+found+your+contact+through+portfolio+site.%0A%0A"
-          target="_blank"
-          rel="noreferrer"
-        >
+        <a href="https://api.whatsapp.com/send/?phone=%2B917706029601&text=Hello+Shubham+I+found+your+contact+through+your+portfolio." target="_blank" rel="noreferrer">
           <img src={whatsapp} alt="Whatsapp" />
         </a>
-        <a
-          href="https://t.me/CodeVinayak"
-          target="_blank"
-          rel="noreferrer"
-        >
+        <a href="https://t.me/Shubhams872" target="_blank" rel="noreferrer">
           <img src={telegram} alt="telegram" />
-        </a></div>
+        </a>
+        <a href="https://www.instagram.com/Shubham.zenith_/" target="_blank" rel="noreferrer">
+          <img src={instagramIcon} alt="Instagram" />
+        </a>
+      </div>
         </ScrollAnimation>
       </div>
       <div className="hero-image">
         <ScrollAnimation animateIn="fadeInRight" delay={1 * 1000}>
-          <img src={Illustration} alt="Ilustração" />
+          <img src={Illustration} alt="Shubham Singh Developer Illustration" />
         </ScrollAnimation>
       </div>
     </Container>
