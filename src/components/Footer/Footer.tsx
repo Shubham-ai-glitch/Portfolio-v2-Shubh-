572891605651375267e20bb57e817719282fafe7
Shubham-ai-glitch <shubhamsingh7236002388@gmail.com>
2026-01-22 14:02:10 +0530

    Footer.tsx

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,5 @@
 import { Container } from './styles'
 import reactIcon from '../../assets/react-icon.svg'
-import linkedin from '../../assets/linkedin.svg'
 import githubIcon from '../../assets/github.svg'
 import whatsapp from '../../assets/whatsapp.svg'
 import telegram from '../../assets/telegram.svg'
@@ -9,50 +8,22 @@ import instagramIcon from '../../assets/instagram.svg'
 export function Footer() {
   return (
     <Container className="footer">
-      <a href="https://vinayaksingh.in" className="logo">
-        <span>www.vinayak</span>
-        <span>singh.in</span>
+      <a href="/" className="logo">
+        <span>www.shubham</span>
+        <span>singh</span>
       </a>
-      <div>
-        <p>
-          This Website was made with <img src={reactIcon} alt="React" />
-          {/* <span>❤️</span> */}
-        </p>
-      </div>
+      <div><p>Made with <img src={reactIcon} alt="React" /></p></div>
       <div className="social-media">
-        <a
-          href="https://www.linkedin.com/in/codevinayak"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img src={linkedin} alt="Linkedin" />
-        </a>
-        <a
-          href="https://github.com/CodeVinayak/"
-          target="_blank"
-          rel="noreferrer"
-        >
+        <a href="https://github.com/Shubhams872-spec" target="_blank" rel="noreferrer">
           <img src={githubIcon} alt="GitHub" />
         </a>
-        <a
-          href="https://api.whatsapp.com/send/?phone=%2B919630576848&text=Hello+Vinayak+I+found+your+contact+through+portfolio+site.%0A%0A"
-          target="_blank"
-          rel="noreferrer"
-        >
+        <a href="https://api.whatsapp.com/send/?phone=%2B919415181609" target="_blank" rel="noreferrer">
           <img src={whatsapp} alt="Whatsapp" />
         </a>
-        <a
-          href="https://t.me/CodeVinayak"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img src={telegram} alt="telegram" />
+        <a href="https://t.me/Shubhams872" target="_blank" rel="noreferrer">
+          <img src={telegram} alt="Telegram" />
         </a>
-        <a
-          href="https://www.instagram.com/vinayaksingh.in"
-          target="_blank"
-          rel="noreferrer"
-        >
+        <a href="https://www.instagram.com/Shubham.zenith_/" target="_blank" rel="noreferrer">
           <img src={instagramIcon} alt="Instagram" />
         </a>
       </div>
