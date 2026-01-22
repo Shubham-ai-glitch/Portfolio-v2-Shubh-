2170060fc30be9c80c0e8a6539373e5c7134fa3f
Shubham-ai-glitch <shubhamsingh7236002388@gmail.com>
2026-01-22 14:01:40 +0530

    Contact.tsx

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -3,24 +3,21 @@ import emailIcon from "../../assets/email-icon.svg";
 import phoneIcon from "../../assets/phone-icon.svg"
 import { Form } from "../Form/Form";
 
-
 export function Contact(){
-
   return(
     <Container id="contact">
       <header>
-        <h2>Contact</h2>
-        <p>Ready to get started on your project? </p>
-        <p>Contact me now for a Free consultation.</p>
+        <h2>Contact Me</h2>
+        <p>Interested in working together or have a query?</p>
       </header>
       <div className="contacts">
         <div>
-        <a href="mailto:vinayak@vinayaksingh.com"><img src={emailIcon} alt="Email" /></a> 
-          <a href="mailto:vinayak@vinayaksingh.com">vinayak@vinayaksingh.com</a>
+          <a href="mailto:shubhamsingh9415191609@gmail.com"><img src={emailIcon} alt="Email" /></a> 
+          <a href="mailto:shubhamsingh9415191609@gmail.com">shubhamsingh9415191609@gmail.com</a>
         </div>
         <div>
-        <a href="tel:+919630576848"><img src={phoneIcon} alt="Phone No" /></a>
-          <a href="tel:+919630576848">(+91) 9630576848</a>
+          <a href="tel:+919415181609"><img src={phoneIcon} alt="Phone No" /></a>
+          <a href="tel:+919415181609">(+91) 9415181609</a>
         </div>  
       </div>
       <Form></Form>
