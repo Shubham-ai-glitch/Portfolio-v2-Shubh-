d64503e4a45708939662583cf2dce89ae252e832
Shubham-ai-glitch <shubhamsingh7236002388@gmail.com>
2026-01-24 08:33:40 +0530

    Contact.tsx

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,24 +1,19 @@
-import { Container } from "./styles";
 import emailIcon from "../../assets/email-icon.svg";
-import phoneIcon from "../../assets/phone-icon.svg"
+import { Container } from "./styles";
 import { Form } from "../Form/Form";
 
-export function Contact(){
-  return(
+export function Contact() {
+  return (
     <Container id="contact">
       <header>
         <h2>Contact Me</h2>
-        <p>Interested in working together or have a query?</p>
+        <p>If you saw my portfolio and want to talk, don't hesitate!</p>
       </header>
       <div className="contacts">
         <div>
           <a href="mailto:shubhamsingh9415191609@gmail.com"><img src={emailIcon} alt="Email" /></a> 
           <a href="mailto:shubhamsingh9415191609@gmail.com">shubhamsingh9415191609@gmail.com</a>
         </div>
-        <div>
-          <a href="tel:+919415181609"><img src={phoneIcon} alt="Phone No" /></a>
-          <a href="tel:+919415181609">(+91) 9415181609</a>
-        </div>  
       </div>
       <Form></Form>
     </Container>
