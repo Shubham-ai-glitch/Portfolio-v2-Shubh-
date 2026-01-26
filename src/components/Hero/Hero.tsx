6dc5967466cd0b20e3674d598b63317caa4954ba
Shubham-ai-glitch <shubhamsingh7236002388@gmail.com>
2026-01-26 17:27:20 +0530

    Hero.tsx

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,38 +1,30 @@
-import { BrowserRouter } from "react-router-dom"
 import { Container } from "./styles"
 import ScrollAnimation from "react-animate-on-scroll"
-import Illustration from "../../assets/illustration.svg"
-import { HashLink } from "react-router-hash-link"
-import Hello from '../../assets/Hello.svg'
-import resume from "../../assets/Shubham_Singh_Resume.pdf"
+import githubIcon from '../../assets/github.svg'
+import instagramIcon from '../../assets/instagram.svg'
+import telegramIcon from '../../assets/telegram.svg'
 
 export function Hero() {
   return (
     <Container id="home">
       <div className="hero-text">
         <ScrollAnimation animateIn="fadeInUp">
-          <p>Hello <img src={Hello} alt="Hello" width="20px"/>, I'm</p>
+          <p>Hello, I'm</p>
         </ScrollAnimation>
-        <ScrollAnimation animateIn="fadeInUp" delay={0.2 * 1000}>
-          <h1>Shubham Singh</h1>
-        </ScrollAnimation>
-        <ScrollAnimation animateIn="fadeInUp" delay={0.4 * 1000}>
-          <h3>Developer & Hacking Enthusiast</h3>
-        </ScrollAnimation>
-        <ScrollAnimation animateIn="fadeInUp" delay={0.6 * 1000}>
-          <p className="small-resume">11th Grade Student | Kanpur</p>
-        </ScrollAnimation>
-        <ScrollAnimation animateIn="fadeInUp" delay={0.8 * 1000}>
-          <BrowserRouter>
-            <HashLink smooth to="#contact" className="button">Contact</HashLink>
-            <a href={resume} download="Shubham_Singh_Resume.pdf" className="button" style={{marginLeft: '1rem'}}>Resume</a>
-          </BrowserRouter>
-        </ScrollAnimation>
-      </div>
-      <div className="hero-image">
-        <ScrollAnimation animateIn="fadeInRight" delay={1 * 1000}>
-          <img src={Illustration} alt="Illustration" />
-        </ScrollAnimation>
+        <h1>Shubham Singh</h1>
+        <h3>Frontend Developer & Hacking Enthusiast</h3>
+        <p className="small-resume">11th Grade Student | NEET Aspirant | Kanpur</p>
+        <div className="social-media">
+          <a href="https://github.com/Shubhams872-spec" target="_blank" rel="noreferrer">
+            <img src={githubIcon} alt="GitHub" style={{width: '50px'}} />
+          </a>
+          <a href="https://www.instagram.com/Shubham.zenith_/" target="_blank" rel="noreferrer">
+            <img src={instagramIcon} alt="Instagram" style={{width: '50px'}} />
+          </a>
+          <a href="https://t.me/shubham_zenith" target="_blank" rel="noreferrer">
+            <img src={telegramIcon} alt="Telegram" style={{width: '50px'}} />
+          </a>
+        </div>
       </div>
     </Container>
   )
