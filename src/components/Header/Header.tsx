16a516f90383ff282948ea0d3e8d36d7478e8f92
Shubham-ai-glitch <shubhamsingh7236002388@gmail.com>
2026-01-26 17:25:56 +0530

    Header.tsx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,6 +2,7 @@ import { Container } from './styles'
 import { BrowserRouter } from 'react-router-dom'
 import { HashLink } from 'react-router-hash-link'
 import { useState } from 'react'
+import resume from "../../assets/Shubham_Singh_Resume.pdf"
 
 export function Header() {
   const [isActive, setActive] = useState(false)
@@ -12,15 +13,15 @@ export function Header() {
     <Container className="header-fixed">
       <BrowserRouter>
         <HashLink smooth to="#home" className="logo">
-          <span>{"<Shubham "}</span>
-          <span>{" Singh/>"}</span>
+          <span>{"<Shubham_"}</span>
+          <span>{"Singh/>"}</span>
         </HashLink>
         <nav className={isActive ? 'active' : ''}>
           <HashLink smooth to="#home" onClick={closeMenu}>Home</HashLink>
           <HashLink smooth to="#about" onClick={closeMenu}>About Me</HashLink>
           <HashLink smooth to="#project" onClick={closeMenu}>Project</HashLink>
           <HashLink smooth to="#contact" onClick={closeMenu}>Contact</HashLink>
-          <a href="https://ig.me/m/Shubham.zenith_?text=Hi%20Shubham,%20I%20reached%20you%20through%20your%20portfolio!" target="_blank" rel="noreferrer" className="button">Instagram</a>
+          <a href={resume} download="Shubham_Singh_Resume.pdf" className="button">Resume</a>
         </nav>
         <div aria-expanded={isActive} className={isActive ? 'menu active' : 'menu'} onClick={toggleActive}></div>
       </BrowserRouter>
