d6ed4f5b7fa55f876e814c741860e26c2d3d47f6
Shubham-ai-glitch <shubhamsingh7236002388@gmail.com>
2026-01-28 08:47:10 +0530

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { About } from './components/About/About'
 import { Project } from './components/Project/Project'
 import { Contact } from './components/Contact/Contact'
 import { Footer } from './components/Footer/Footer'
-import { Music } from './components/Music/Music' // Music import kiya
+import { Music } from './components/Music/Music'
 
 function App() {
   const [isDark, setIsDark] = useState(true)
@@ -18,13 +18,17 @@ function App() {
   return (
     <>
       <GlobalStyle isDark={isDark} />
+      {/* Header ko theme function aur state bhej rahe hain */}
       <Header toggleTheme={toggleTheme} isDark={isDark} />
+      
+      {/* ERROR YAHAN THA: Hero ko isDark state bhejni zaroori hai */}
       <Hero isDark={isDark} />
+      
       <About />
       <Project />
       <Contact />
       <Footer />
-      <Music /> {/* Music button yahan add kiya */}
+      <Music />
     </>
   )
 }
