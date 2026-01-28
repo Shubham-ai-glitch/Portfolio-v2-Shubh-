9eebc09b0b0d8810fcb9e0c7e95ce527460fe0df
Shubham-ai-glitch <shubhamsingh7236002388@gmail.com>
2026-01-28 09:04:51 +0530

    Hero.tsx

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -3,13 +3,10 @@ import { Container } from "./styles"
 import ScrollAnimation from "react-animate-on-scroll"
 import Illustration from "../../assets/illustration.svg"
 import { NavHashLink } from "react-router-hash-link"
-import linkedin from '../../assets/linkedin.svg'
 import githubIcon from '../../assets/github.svg'
-import whatsapp from '../../assets/whatsapp.svg'
 import Hello from '../../assets/Hello.gif'
 import telegram from '../../assets/telegram.svg'
 
-// Yahan '?' lagane se error khatam ho jayega
 export function Hero({ isDark = true }: { isDark?: boolean }) {
   return (
     <Container id="home">
@@ -33,14 +30,8 @@ export function Hero({ isDark = true }: { isDark?: boolean }) {
         </ScrollAnimation>
         <ScrollAnimation animateIn="fadeInUp" delay={1 * 1000}>
           <div className="social-media">
-            <a href="https://www.linkedin.com/in/shubham-singh-030a54345" target="_blank" rel="noreferrer">
-              <img src={linkedin} alt="Linkedin" />
-            </a>
             <a href="https://github.com/Shubham-ai-glitch" target="_blank" rel="noreferrer">
               <img src={githubIcon} alt="GitHub" />
-            </a>
-            <a href="https://wa.me/917007563148" target="_blank" rel="noreferrer">
-              <img src={whatsapp} alt="Whatsapp" />
             </a>
             <a href="https://t.me/Shubhamsingh_70" target="_blank" rel="noreferrer">
               <img src={telegram} alt="telegram" />
